9826d433e2c06cea29483270542f65513dab2f8b
Amy Quinn <amyequinn@hotmail.co.uk>
2019-05-16 23:06:56 +0100

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,11 @@
+class Author
+
+attr_accessor :name
+
+def instantize(name)
+
+  @name = name
+
+end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+
+attr_accessor :title, :author
+
+def instantize(title, author)
+  @title = title
+  @author = author
+
+end
+
+
+
+end
